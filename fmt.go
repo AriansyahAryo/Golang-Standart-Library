3d7271cf666477b93f106afad219482fa7ab34eb
ariansyaharyo <ariansyaharyo@gmail.com>
2023-11-14 18:57:23 +0700

    library fmt

---
+++ fmt.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Ariansyah"
+	lastName := "Aryo"
+
+	fmt.Println("Hello", firstName, lastName)
+	fmt.Println("Hello'", firstName, lastName, "'")
+	//	disubtitusikan biar gk ada jarak spasinya
+	fmt.Printf("Hello '%s %s'\n", firstName, lastName)
+}
